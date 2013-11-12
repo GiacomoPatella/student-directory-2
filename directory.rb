47e3898d957362fe038a1af6b415ec4f617e0a8d
Ericat <erica.salvaneschi@hotmail.com>
2013-11-12 15:08:09 +0000

    Added user input

--- directory.rb
+++ directory.rb
@@ -1,34 +1,30 @@
-students = [
-  {:name => "Chris", :cohort => :november},
-  {:name => "Anath", :cohort => :november},
-  {:name => "Simon", :cohort => :november},
-  {:name => "Hannah", :cohort => :november},
-  {:name => "Ken", :cohort => :november},
-  {:name => "Asta", :cohort => :november},
-  {:name => "G", :cohort => :november}
-]
-
-
 def print_header
-	puts "Lovely folks of my cohort at Makers Academy:"
-	puts "-----------"
+        puts "Lovely folks of my cohort at Makers Academy:"
+        puts "-----------"
 end
 
-def print(students)
-	students.each do |student|
-		puts "#{student[:name]} (#{student[:cohort]} cohort)"
-	end
+def input_students
+  puts "Type the names of the students:"
+  puts "To finish just hit return twice!"
+
+  students = []
+
+  name = gets.chomp
+
+  while !name.empty? do
+  students << { :name => name, :cohort => :november }
+  puts "Now we have #{students.length} students"
+  name = gets.chomp
+  end
+  students #if you comment this out: directory.rb:23:in `print_footer': undefined method `length' for nil:NilClass (NoMethodError)
+  #necessary to return the array
 end
 
 def print_footer(students)
-	puts "Overall we have #{students.length} students!"
+        puts "Overall we have #{students.length} students!"
 end
 
-
-print_header
+students = input_students
 print(students)
 print_footer(students)
-
-
-
 
